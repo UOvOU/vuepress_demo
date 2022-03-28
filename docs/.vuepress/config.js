9e0fcf51caa3a2cba18a764d32490e6ade10cde6
nora.lin <nora.lin@dji.com>
2022-03-29 06:41:48 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,30 +8,14 @@ module.exports = {
   theme: "@vuepress/theme-default",
   themeConfig: {
     logo: "https://vuejs.org/images/logo.png",
+    docsRepo: "https://github.com/noralinlll/vuepress_demo",
+    docsBranch: "doc-release",
+    docsDir: "docs",
+    editLinkPattern: ":repo/-/edit/:branch/:path",
+    editLinkText: "Github EditLink",
   },
+  base: "/vuepress_demo/",
+  editLnk: true,
   extractHeaders: true,
   toc: true,
-  themeConfig: {
-    // 侧边栏数组
-    // 所有页面会使用相同的侧边栏
-    sidebar: [
-      // SidebarItem
-      {
-        text: "Foo",
-        link: "/foo/",
-        children: [
-          // SidebarItem
-          {
-            text: "github",
-            link: "https://github.com",
-            children: [],
-          },
-          // 字符串 - 页面文件路径
-          "/foo/bar.md",
-        ],
-      },
-      // 字符串 - 页面文件路径
-      "/bar/README.md",
-    ],
-  },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,1 @@
-Hello
+nora
